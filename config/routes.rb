a3d9ab12556aa3bf9f47dbaf5269df5e4eceb252
Simon Kisimov <skisimov@gmail.com>
2017-03-16 14:42:16 +0300

    add devise gem, update devise form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root "home#index"
+
+devise_for :users
+root to: "home#index"
+
+get 'home/index'
+ 
 
 get 'about' => 'pages#about'
 
