eb238c847434b58707e0c3ae6dfb151f56cfc36c
Simon Kisimov <skisimov@gmail.com>
2017-03-20 16:13:12 +0300

    add articles and comments controllers

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,14 @@ root to: "home#index"
 
 get 'home/index'
  
-
 get 'about' => 'pages#about'
 
+ resources :articles 
+
  resource :contacts, only: [:new, :create], path_names: { :new => '' }
+  
+resources :articles do
+	resources :comments
+end
+
 end
