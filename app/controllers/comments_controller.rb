fb268a2cfbab2df6b633fd01ea583e3fe44c0c54
Simon Kisimov <skisimov@gmail.com>
2017-03-20 17:28:57 +0300

    add comment author

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,19 @@
-class CommentsController < ApplicationController
- 
+ class CommentsController < ApplicationController
+  before_filter :authenticate_user!
+
   def create
     @article = Article.find(params[:article_id])
-    @article.comments.create(comment_params)
+    #@article.comments.create(comment_params)
+    @comment = @article.comments.new do |c|
+      c.author = current_user.username
+      c.body = comment_params[:body]
+    end
+    @comment.save
 
     redirect_to article_path(@article)
   end
 
-  private
+  private 
 
   def comment_params
     params.require(:comment).permit(:author, :body)
