d5c22d93cd98800cddbaa7c13b00dafebd2a4da9
Simon Kisimov <skisimov@gmail.com>
2017-04-03 17:46:10 +0300

    change prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default :charset => "utf-8"
   # Setup for production - deliveries, no errors raised
-  config.action_mailer.default :charset => "utf-8"
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
