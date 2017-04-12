24be141901be6171fb9d805a57a4b9855eb550d8
Simon Kisimov <skisimov@gmail.com>
2017-04-12 13:26:44 +0300

    fix controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,22 @@
- class CommentsController < ApplicationController
-  before_filter :authenticate_user!
+class CommentsController < ApplicationController
+
+before_filter :authenticate_user!
 
   def create
     @article = Article.find(params[:article_id])
-    #@article.comments.create(comment_params)
+
     @comment = @article.comments.new do |c|
       c.author = current_user.username
       c.body = comment_params[:body]
     end
-    @comment.save
-
-    redirect_to article_path(@article)
+      @comment.save
+      redirect_to article_path(@article)
   end
 
   private 
 
   def comment_params
-    params.require(:comment).permit(:author, :body)
+  params.require(:comment).permit(:author, :body)
   end
 
 end
